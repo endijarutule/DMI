7cd14693d2dcd3bbe7f6dce05c303935bd7f9fc9
aijatikuma@inbox.lv <rutule8@inbox.lv>
2017-11-09 17:24:28 +0200

    20171109_17_55

---
+++ PYTHON/hello.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+'''
+print "Hello World!"
+'''
+
+#https://www.tutorialspoint.com/unix_commands/echo.htm
+# Example 3. - 7.
+
+# example 3
+print "Here \bthe \bspaces \bare \bbackspaced."
+# example 4
+print "Here \nthe \nspaces \nare \nnewLined."
+# example 5
+print "Here \tthe \tspaces \thave \thorizontal  \ttab \tspaces."
+# example 6
+print "Here \vthe \vspaces \vhave \vvertical  \vtab \vspaces."
+# example 7
+print "Here the \rcontent before this is removed."
+ 
+
+
+
+
+
